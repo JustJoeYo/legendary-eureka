b0232da2ad158a6fa878388e4d8f72aa3abe7534
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-03-10 08:07:09 -0700

    file structure

---
+++ spec/ride_spec.rb
@@ -0,0 +1,29 @@
+require_relative 'spec_helper'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+RSpec.describe Ride do
+  before(:each) do
+
+  end
+
+  describe 'initialization' do
+    it '#initialize' do
+
+    end
+
+    it '#attributes' do
+
+    end
+  end
+
+  describe 'instance methods' do
+
+  end
+
+  describe 'class methods' do
+
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'pry'
+require 'rspec'
+
+require './lib/bike_club.rb'
+require './lib/biker.rb'
+require './lib/ride.rb'
