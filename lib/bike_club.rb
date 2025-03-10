16ef91130af03587901cb8893bb8f4eb0bb60de5
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-03-10 08:28:39 -0700

    finished bike club class

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -14,5 +14,14 @@ class BikeClub
     @bikers.max_by { |biker| biker.rides.values.flatten.size }
   end
 
-  
+  def best_time(ride)
+    eligible_bikers = @bikers.select { |biker| biker.rides[ride] }
+    eligible_bikers.min_by { |biker| biker.personal_record(ride) }
+  end
+
+  def bikers_eligible(ride)
+    @bikers.select do |biker|
+      biker.acceptable_terrain.include?(ride.terrain) && ride.total_distance <= biker.max_distance
+    end
+  end
 end
