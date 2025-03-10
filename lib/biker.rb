ea0085387ce7f56bec9fb6a7e9fa8c3228a02168
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-03-10 08:19:31 -0700

    biker class updates

--- lib/biker.rb
+++ lib/biker.rb
@@ -7,4 +7,25 @@ class Biker
     @rides = {}
     @acceptable_terrain = []
   end
+
+  def learn_terrain(terrain)
+    @acceptable_terrain << terrain
+  end
+
+  def log_ride(ride, time)
+    # tempted to use return unless @acceptable_terrain.include?(ride.terrain) && ride.total_distance <= @max_distance instead of if statements
+    if @acceptable_terrain.include?(ride.terrain) && ride.total_distance <= @max_distance
+      @rides[ride] ||= []
+      @rides[ride] << time
+    end
+  end
+
+  def personal_record(ride)
+    # once again return false unless @rides[ride]
+    if @rides[ride]
+      @rides[ride].min
+    else
+      false
+    end
+  end
 end
